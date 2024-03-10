446dfeb6ee89314daed9b1035100946947727d91
Gabriel <gabri.mevial@gmail.com>
2024-03-10 18:47:33 -0300

    corrigindo requisitos 3 e 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,24 @@
+def sort_string(s):
+    if len(s) <= 1:
+        return s
+
+    pivot = s[0]
+    less = ''.join(char for char in s[1:] if char <= pivot)
+    greater = ''.join(char for char in s[1:] if char > pivot)
+
+    return sort_string(less) + pivot + sort_string(greater)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui./"""
-    raise NotImplementedError
+    if not first_string or not second_string:
+        return first_string, second_string, False
+
+    first_string = first_string.lower().replace(" ", "")
+    second_string = second_string.lower().replace(" ", "")
+
+    # Ordena as strings
+    sorted_first = sort_string(first_string)
+    sorted_second = sort_string(second_string)
+    are_anagrams = sorted_first == sorted_second
+
+    return sorted_first, sorted_second, are_anagrams
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-
+    if word is None or len(word) == 0:
+        return False
+    if high_index is None:
+        high_index = len(word) - 1
     if low_index >= high_index:
         return True
     if word[low_index] != word[high_index]:
