00662f09eee56f789ba633b86302ae660c61c601
Gabriel <gabri.mevial@gmail.com>
2024-03-10 18:13:17 -0300

    requisito 2 completo

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,13 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("hello", 3) == "leh_ol"
+    assert encrypt_message("hello", 2) == "oll_eh"
+    assert encrypt_message("hello", 8) == "olleh"
+    with pytest.raises(TypeError):
+        encrypt_message(123, 3)
+    with pytest.raises(TypeError):
+        encrypt_message("hello", "key")
+    assert encrypt_message("hello", 20) == "olleh"
