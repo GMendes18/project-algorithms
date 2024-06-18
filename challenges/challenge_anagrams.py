a2cb90cf5184537a8d9e10a6c6d443fb2d450f22
Gabriel Mendes <gabri.mevial@gmail.com>
2024-06-18 17:04:14 -0300

    test

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,24 +1,51 @@
-def sort_string(s):
-    if len(s) <= 1:
-        return s
+def merge_sort(s):
+    if len(s) > 1:
+        mid = len(s) // 2
+        left_half = s[:mid]
+        right_half = s[mid:]
 
-    pivot = s[0]
-    less = ''.join(char for char in s[1:] if char <= pivot)
-    greater = ''.join(char for char in s[1:] if char > pivot)
+        merge_sort(left_half)
+        merge_sort(right_half)
 
-    return sort_string(less) + pivot + sort_string(greater)
+        i = j = k = 0
+
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                s[k] = left_half[i]
+                i += 1
+            else:
+                s[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            s[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            s[k] = right_half[j]
+            j += 1
+            k += 1
 
 
 def is_anagram(first_string, second_string):
     if not first_string or not second_string:
         return first_string, second_string, False
 
+    # Convertendo para minúsculas e removendo espaços em branco
     first_string = first_string.lower().replace(" ", "")
     second_string = second_string.lower().replace(" ", "")
 
-    # Ordena as strings
-    sorted_first = sort_string(first_string)
-    sorted_second = sort_string(second_string)
-    are_anagrams = sorted_first == sorted_second
+    # Convertendo as strings em listas de caracteres
+    first_list = list(first_string)
+    second_list = list(second_string)
 
-    return sorted_first, sorted_second, are_anagrams
+    # Ordenando as listas usando merge sort
+    merge_sort(first_list)
+    merge_sort(second_list)
+
+    # Verificando se as listas ordenadas são iguais
+    are_anagrams = first_list == second_list
+
+    return ''.join(first_list), ''.join(second_list), are_anagrams
